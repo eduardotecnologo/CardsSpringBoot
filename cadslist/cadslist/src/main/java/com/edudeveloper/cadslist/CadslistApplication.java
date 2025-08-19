9d92e17ae23638db03777d6679a1381fb589f333
eduardotecnologo <eduardotecnologo@hotmail.com>
2025-08-18 21:47:38 -0300

    feat: create developer

---
+++ cadslist/cadslist/src/main/java/com/edudeveloper/cadslist/CadslistApplication.java
@@ -0,0 +1,13 @@
+package com.edudeveloper.cadslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadslistApplication.class, args);
+	}
+
+}
---
+++ cadslist/cadslist/src/test/java/com/edudeveloper/cadslist/CadslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudeveloper.cadslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
